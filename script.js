00f435f0b089ec60df25539ef23dc9e814985f91
mbene dieye <mbenedieye757@gmail.com>
2025-05-05 22:27:16 +0200

    genera numeri pari

--- script.js
+++ script.js
@@ -1,28 +1,35 @@
-let maVariable= "nome";
-const maConstante= 3.14;
+// let maVariable= "nome";
+// const maConstante= 3.14;
 
-console.log(maVariable);
-console.log(maConstante);
+// console.log(maVariable);
+// console.log(maConstante);
 
-let num = 5;
-console.log(num);
+// let num = 5;
+// console.log(num);
 
- num = 9;
- console.log(num);
+//  num = 9;
+//  console.log(num);
 
 
- let number = 2;
- console.log(number);
- {
-    let number = 8;
-    console.log(number);
- }
+//  let number = 2;
+//  console.log(number);
+//  {
+//     let number = 8;
+//     console.log(number);
+//  }
 
- let outside = 7;
- console.log(outside);
- console.log(inside);
- {
-    let inside = 4;
-    console.log(outside);
-    console.log(inside);
+//  let outside = 7;
+//  console.log(outside);
+// //  console.log(inside);
+//  {
+//     let inside = 4;
+//     console.log(outside);
+//     console.log(inside);
+//  }
+
+  
+ for(i=0;i<=20;i++){
+    if(i%2===0){
+        console.log(i)
+    }
  }
