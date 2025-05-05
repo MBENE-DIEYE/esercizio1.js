025e32124e23a451db0addd88225daefb13ec299
mbene dieye <mbenedieye757@gmail.com>
2025-05-05 22:18:08 +0200

    inside

--- script.js
+++ script.js
@@ -4,16 +4,25 @@ const maConstante= 3.14;
 console.log(maVariable);
 console.log(maConstante);
 
-let num = 5
-console.log(num)
+let num = 5;
+console.log(num);
 
- num = 9
- console.log(num)
+ num = 9;
+ console.log(num);
 
 
- let number = 2
- console.log(number)
+ let number = 2;
+ console.log(number);
  {
-    let number = 8
-    console.log(number)
+    let number = 8;
+    console.log(number);
+ }
+
+ let outside = 7;
+ console.log(outside);
+ console.log(inside);
+ {
+    let inside = 4;
+    console.log(outside);
+    console.log(inside);
  }
